a61cfe674b3384f4c28f54107320fbbccb509186
Jeff Labonte <jeff.labonte12@gmail.com>
2020-07-26 19:51:15 -0400

    Change list for dict

--- home_infra_manager/settings.py
+++ home_infra_manager/settings.py
@@ -122,10 +122,10 @@ STATIC_URL = '/static/'
 
 # Django Rest Framework
 
-REST_FRAMEWORK = [
+REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-]
+    ],
+}
