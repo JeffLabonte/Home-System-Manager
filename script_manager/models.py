21a8a3a63027f8a1b299d8c9550d03adf812ff8a
Jeff Labonte <grimsleepless@protonmail.com>
2020-07-26 20:39:57 -0400

    Create Script and ScriptRevision models

--- script_manager/models.py
+++ script_manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Script(models.Model):
+    name = models.CharField(max_length=64, required=True)
+    script = models.ForeignKey(
+        'ScriptRevision',
+        on_delete=models.CASCADE,
+    )
+    last_change = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ScriptRevision(models.Model):
+    script_file = models.FileField(upload_to='scripts/')
+    revision = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
