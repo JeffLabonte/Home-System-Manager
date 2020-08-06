022228cd74b1d31d577156a103361d896a466cf6
Jeff Labonte <grimsleepless@protonmail.com>
2020-08-05 21:19:49 -0400

    Create configs to run postgres

--- home_infra_manager/settings.py
+++ home_infra_manager/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'home_infra_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DBNAME'),
+        'USER': os.getenv('DBUSER'),
+        'PASSWORD': os.getenv('DBPASSWORD'),
+        'HOST': os.getenv('DBHOST', 'localhost'),
+        'PORT': os.getenv('DBPORT', '5432'),
+    },
 }
 
 
