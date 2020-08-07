b3783abbb997b9ff7d8afec382f72b3583b86d59
Jeff Labonte <grimsleepless@protonmail.com>
2020-08-07 03:16:11 -0400

    Add script to env.template
    This is meant to create your configurations if you want to participate and expose yourself.

--- envs/env.template.sh
+++ envs/env.template.sh
@@ -10,3 +10,10 @@ DB_PORT='5432'
 DB_USER='Update ME'
 DB_PASSWORD='Update ME'
 
+sed -i "s/DB_NAME/${DB_NAME}/g" docker-compose.yml
+sed -i "s/DB_HOST/${DB_HOST}/g" docker-compose.yml
+sed -i "s/DB_PORT/${DB_PORT}/g" docker-compose.yml
+sed -i "s/DB_USER/${DB_USER}/g" docker-compose.yml
+sed -i "s/DB_PASSWORD/${DB_PASSWORD}/g" docker-compose.yml
+
+sed -i "s/SECRET_DJANGO_KEY/${SECRET_DJANGO_KEY}" docker-compose.yml
