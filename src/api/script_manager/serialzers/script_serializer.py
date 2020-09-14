eb43ed8465e88adc5e2fd7f71f783ab3e4d7b801
Jeff Labonte <grimsleepless@protonmail.com>
2020-09-14 00:07:47 -0400

    Update serializer and create new one for ScriptRevision

--- src/api/script_manager/serialzers/script_serializer.py
+++ src/api/script_manager/serialzers/script_serializer.py
@@ -1,12 +1,38 @@
 from rest_framework import serializers
-from api.script_manager.models import Script
+
+from api.script_manager.models import Script, ScriptRevision
+from api.script_manager.models import FILE_TYPE_CHOICES
 
 
 class ScriptSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        required=True,
+        max_length=64,
+        min_length=12,
+    )
+    file_type = serializers.ChoiceField(
+        choices=FILE_TYPE_CHOICES,
+        required=True,
+    )
+
     class Meta:
         model = Script
         fields = [
             'name',
             'script',
             'file_type',
+            'last_date_modified',
+            'creation_date',
+        ]
+        read_only_fields = [
+            'last_date_modified'
+            'creation_date ',
+        ]
+
+
+class ScriptRevisionSerializer(serializers.ModelField):
+    class Meta:
+        model = ScriptRevision
+        fields = [
+
         ]
