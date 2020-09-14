6f053993b112e4c0fbf89ff77e43533b0ef20915
Jeff Labonte <grimsleepless@protonmail.com>
2020-09-14 00:07:03 -0400

    Update ViewSet query to take change into account

--- src/api/script_manager/views/script_view.py
+++ src/api/script_manager/views/script_view.py
@@ -4,6 +4,6 @@ from api.script_manager.models import Script
 
 
 class ScriptViewSet(viewsets.ModelViewSet):
-    queryset = Script.objects.all().order_by('last_change')
+    queryset = Script.objects.all().order_by('last_date_modified')
     serializer_class = ScriptSerializer
     permission_classes = []
