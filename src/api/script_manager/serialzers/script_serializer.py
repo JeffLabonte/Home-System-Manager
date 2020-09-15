fa84c324b62850ed7dccaae480013d616af563ea
Jeff Labonte <grimsleepless@protonmail.com>
2020-09-14 23:08:55 -0400

    Create Serializer for ScriptRevision

--- src/api/script_manager/serialzers/script_serializer.py
+++ src/api/script_manager/serialzers/script_serializer.py
@@ -1,7 +1,29 @@
+from uuid import uuid4
+
 from rest_framework import serializers
 
 from api.script_manager.models import Script, ScriptRevision
 from api.script_manager.models import FILE_TYPE_CHOICES
+
+
+class ScriptRevisionSerializer(serializers.ModelSerializer):
+
+    id = serializers.UUIDField(default=uuid4)
+    script_file = serializers.URLField(required=True)
+    revision = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = ScriptRevision
+        fields = [
+            'id',
+            'script_file',
+            'revision',
+        ]
+        default_fields = [
+            'id',
+            'script_file',
+            'revision',
+        ]
 
 
 class ScriptSerializer(serializers.ModelSerializer):
@@ -27,12 +49,4 @@ class ScriptSerializer(serializers.ModelSerializer):
         read_only_fields = [
             'last_date_modified'
             'creation_date ',
-        ]
-
-
-class ScriptRevisionSerializer(serializers.ModelField):
-    class Meta:
-        model = ScriptRevision
-        fields = [
-
         ]
