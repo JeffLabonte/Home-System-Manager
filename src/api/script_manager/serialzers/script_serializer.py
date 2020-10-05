c073c0ef8351f311d74ba87c047030457c2f5cdd
Jeff Labonte <grimsleepless@protonmail.com>
2020-10-05 00:14:32 -0400

    WIP - Work Serializer to match models

--- src/api/script_manager/serialzers/script_serializer.py
+++ src/api/script_manager/serialzers/script_serializer.py
@@ -2,27 +2,68 @@ from uuid import uuid4
 
 from rest_framework import serializers
 
-from api.script_manager.models import Script, ScriptRevision
+from api.script_manager.models import Script, ScriptExecution, GitRepo
 from api.script_manager.models import FILE_TYPE_CHOICES
 
 
-class ScriptRevisionSerializer(serializers.ModelSerializer):
+class GitRepoSerializer(serializers.ModelSerializer):
 
-    id = serializers.UUIDField(default=uuid4)
-    script_file = serializers.FileField(required=True)
+    id = serializers.UUIDField(
+        default=uuid4
+    )
+    repo_url = models.CharField(
+        max_lenth=128,
+        min_length=1,
+        blank=False,
+        null=False,
+        unique=True,
+    )
     revision = serializers.IntegerField(required=True)
 
     class Meta:
-        model = ScriptRevision
+        model = GitRepo
         fields = [
             'id',
             'script_file',
             'revision',
+            'last_date_modified',
+            'creation_date'
         ]
         default_fields = [
             'id',
             'script_file',
             'revision',
+        ]
+        read_only_fields = [
+            'id',
+        ]
+
+
+class ScriptExecutionSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(
+        default=uuid4,
+        required=True,
+        read_only=True,
+    )
+    git_repo = GitRepoSerializer()
+    script_to_execute = models.CharField(
+        min_length=1,
+        blank=False,
+        null=False,
+    )
+    arguments = models.CharField(
+        min_length=1,
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        model = ScriptExecution
+        fields = [
+            'id',
+            'git_repo',
+            'script_to_execute',
+            'arguments',
         ]
 
 
@@ -31,13 +72,6 @@ class ScriptSerializer(serializers.ModelSerializer):
         required=True,
         max_length=64,
         min_length=1,
-    )
-
-    script = ScriptRevisionSerializer()
-
-    file_type = serializers.ChoiceField(
-        choices=FILE_TYPE_CHOICES,
-        required=True,
     )
 
     def create(self, validated_data):
