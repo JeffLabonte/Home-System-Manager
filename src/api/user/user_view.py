fc6df0ce07b14591c1a68eee6c4efc89cb5559c6
Jeff Labonte <grimsleepless@protonmail.com>
2020-11-11 22:58:46 -0500

    Update user api

--- src/api/user/user_view.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-
-from api.user.user_serializer import UserSerializer
-
-
-class UserViewsets(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
