bc254777c1a3697156d0c9ea31bcd653bfbf3b36
Jeff Labonte <grimsleepless@protonmail.com>
2020-12-25 18:48:43 -0500

    CleanUp

--- src/api/script_manager/views/script_view.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, permissions
-from api.script_manager.serialzers.script_serializer import ScriptSerializer
-from api.script_manager.models import Script
-
-
-class ScriptViewSet(viewsets.ModelViewSet):
-    queryset = Script.objects.all().order_by('last_date_modified')
-    serializer_class = ScriptSerializer
-    permission_classes = [permissions.IsAuthenticated]
--- src/tests/integrations/api/script_manager/test_script_manager_views.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse
-
-
-def test__script_manager_view_not_auth__should_return_401(client):
-    url = reverse('script-list')
-    assert url
-    assert client
